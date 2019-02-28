2231d106d31941b2f30d73d4baccf8528111b820
ch2isk4kos <ch2isk4kos@gmail.com>
2019-02-28 16:46:14 -0500

    pishing complete project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :dogs
+    get '/dogs/sort', to: "dogs#sort"
+    get '/dogs/:id', to: "dogs#show", as: "dog"
+
+  resources :dogs, only: [:index]
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
