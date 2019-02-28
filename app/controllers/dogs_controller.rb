173f7e1304a3bd77fdd8d18b4dea52ce23ebd190
Shemar Dacosta <dshemar6@gmail.com>
2019-02-28 15:48:44 -0500

    eh

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,28 @@
 class DogsController < ApplicationController
+
+
+  def index
+    if params[:sorted] == "true"
+      # Dog.sort_dog
+      byebug
+      # Dog.all.sort_by do |dog|
+      #   dog.employees.length
+      # end
+    end
+    @dogs = Dog.all
+  end
+
+  def show
+    get_dog
+
+  end
+
+  private
+  def get_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+  end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,44 @@
 class EmployeesController < ApplicationController
+
+  before_action :get_employee, only: [:edit, :show, :update]
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    @employee.update(employee_params)
+    redirect_to employee_path(@employee)
+  end
+
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    if @employee.valid?
+      redirect_to employee_path(@employee)
+    else
+      render :new
+    end
+    # @employee.dog_id = 1
+    # @employee.save
+    # byebug
+  end
+
+  private
+
+  def get_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+  end
 end
